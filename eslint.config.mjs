b43837cec30f36ed974a1dd31bf118b586c763ac
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2024-12-13 15:42:28 +0200

    lint: setup react and react-hooks linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintPluginTestingLibrary from 'eslint-plugin-testing-library';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config({
   extends: [
@@ -10,12 +12,18 @@ export default tseslint.config({
     tseslint.configs.stylistic,
     eslintPluginPrettierRecommended,
     eslintPluginTestingLibrary.configs['flat/react'],
+    eslintPluginReact.configs.flat.recommended,
   ],
+  plugins: {
+    'react-hooks': eslintPluginReactHooks,
+  },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-require-imports': 'off',
+    'react/react-in-jsx-scope': 'off',
+    ...eslintPluginReactHooks.configs.recommended.rules,
   },
 });
